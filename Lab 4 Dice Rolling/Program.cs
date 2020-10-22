ccc39f8fc8ab39e55d6f53d2052189e93066f863
Verkeia <verkeiac@yahoo.com>
2020-10-21 22:03:47 -0400

    Lab 4 Dice Rolling

---
+++ Lab 4 Dice Rolling/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Lab_4_Dice_Rolling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Console.WriteLine("Welcome to Grand Circus Casino\nEnter the number of sides for a pair of dice:");
+
+            string numSides = Console.ReadLine();
+
+            int sides;
+            bool realInteger = int.TryParse(numSides, out sides);
+
+            bool keepPlaying = true;
+
+            int rollCount = 1;
+            do
+            {
+               
+
+                Console.WriteLine($"Roll {rollCount}: ");
+
+                Console.WriteLine(RandomNUmber(2*sides));
+
+                
+
+                rollCount++;
+
+            } while (keepPlaying = ContinueGame());
+
+
+
+        }
+
+        static int RandomNUmber(int x)
+
+        {
+
+            Random random = new Random();
+
+            int randomNUmber = random.Next(2, x);
+
+            return randomNUmber;
+        }
+        
+
+        static bool ContinueGame()
+
+        {
+
+            bool continueGame = true;
+            do
+            {
+
+                Console.WriteLine(" Do you want to continue? (Y/N)");
+
+                string userInput = Console.ReadLine();
+
+                userInput = userInput.ToLower();
+                if (userInput.Equals("y"))
+                {
+                    continueGame = true;
+                    break;
+                }
+
+                else if (userInput.Equals("n"))
+                {
+
+                    continueGame = false;
+                    break;
+                }
+
+                else
+                {
+                    Console.WriteLine("Invalid Input try again.");
+                    continue;
+                }
+
+              
+            } while (true);
+            return continueGame;
+
+        }
+    }
+}
